696b9f7745d8796e38769110c0378e2bad3151e0
Eric Xu <avatar.xu@gmail.com>
2020-06-10 14:56:57 +0800

    start UI,and background

--- Assets/Scripts/HomeAction.cs
+++ Assets/Scripts/HomeAction.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class HomeAction : MonoBehaviour
 {
+    public Button StartButton;
+
     // Start is called before the first frame update
     void Start()
     {
+        //StartButton = GameObject.Find("StartButton");
+        //StartButton.onClick.AddListener(ClickStartButton);
+        // SceneManager.LoadScene(0);
+    }
+
+    public void ClickStartButton()
+    {
+        Debug.Log("Start to scene 0");
         SceneManager.LoadScene(0);
     }
 
